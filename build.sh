1469dd548f82f7c3a7bbe8bb471ce8a93d09db07
John <john@doxel.ai>
2021-11-13 18:44:48 +0900

    Hopefully a repeatable build

--- build.sh
+++ build.sh
@@ -7,14 +7,28 @@ JOBS=1
 MYDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 stage1() {
+cd $MYDIR
 sh $MYDIR/sys/unix/setup.sh
+echo Buiding utils
+cd $MYDIR/util
 make CC=$EMCC -j$JOBS
+make CC=emcc lev_comp.js
+make CC=emcc dgn_comp.js
 
-#make install CC=$EMCC PREFIX=$MYDIR/build
-#rm $MYDIR/build/nethack/nethack
-#rm $MYDIR/build/nethack/recover
-#cp $MYDIR/web/nethackrc.default $MYDIR/build/nethack/
-
+echo Buiding everything
+cd $MYDIR
+# hack: make build succeed by putting a dummy nethack file in place
+rm -rf $MYDIR/src/nethack
+touch $MYDIR/src/nethack
+make CC=$EMCC -j$JOBS
+make install CC=$EMCC PREFIX=$MYDIR/build
+# then remove it
+rm -rf $MYDIR/src/nethack
+# and copy the nethack directory in place so it can be bundled.
+rm -rf $MYDIR/src/nethack
+cp -rf $MYDIR/build/nethack $MYDIR/src/nethack
+# and build again with everything bundled
+make CC=$EMCC -j$JOBS
 }
 
 stage2() {
